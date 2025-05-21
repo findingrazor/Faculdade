a9a129fdd521cfee81416db4c55f3dd1fba6885e
clarisse <clariamalotti@gmail.com>
2025-05-21 08:35:45 -0300

    Update Grupo05_Exerc04.sql

--- Bancos de Dados I/Grupo05_Exerc04.sql
+++ Bancos de Dados I/Grupo05_Exerc04.sql
@@ -1,5 +1,5 @@
 /*
-Exercicio 04 - SELECT ATÉ HAVING
+Exercicio 04 - SELECT ATÃ‰ HAVING
 
 Nome: GRUPO 5 - Camila Fortes, Clarisse Amaral e Yanamel Ferreira
 
@@ -17,13 +17,13 @@ HAVING SUM(P.vlrtotal_pedido) > 1100
 
 
 --2)
-SELECT C.categoria, PR.produto, AVG(pd.preco_unit * pd.qtde ) AS Media
+SELECT C.categoria, PR.produto, AVG(pd.preco_unit * pd.qtde - pd.desconto) AS Media
 FROM categoria C INNER JOIN produto PR ON C.codcategoria = PR.codcategoria
 	INNER JOIN pedido_detalhe PD ON PR.codproduto = PD.codproduto
 	INNER JOIN pedido PE ON PE.codpedido = PD.codpedido
 WHERE YEAR(PE.dt_pedido) = 1997  
 GROUP BY PR.produto, C.categoria
-HAVING AVG(pd.preco_unit * pd.qtde  )> 1000
+HAVING AVG(pd.preco_unit * pd.qtde - pd.desconto)> 1000
 ORDER BY Media;
 
 
@@ -33,8 +33,8 @@ ORDER BY Media;
 SELECT Cl.pais, C.categoria, SUM(pd.preco_unit * pd.qtde * (1 - pd.desconto)) AS 'TotalGasto'
 FROM cliente Cl INNER JOIN pedido P ON Cl.codcliente = P.codcliente
 	INNER JOIN pedido_detalhe pd ON P.codpedido = pd.codpedido
-	INNER JOIN produto ON produto.codproduto = pd.codproduto
-	INNER JOIN categoria c ON c.codcategoria = produto.codcategoria
+	INNER JOIN produto pr ON pr.codproduto = pd.codproduto
+	INNER JOIN categoria c ON c.codcategoria = pr.codcategoria
 WHERE MONTH(P.dt_pedido) = 3 AND YEAR(P.dt_pedido) = 1997
 GROUP BY Cl.pais, C.categoria
 HAVING SUM(pd.preco_unit * pd.qtde * (1 - pd.desconto)) < 100
