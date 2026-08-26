03e74b5e00b2b3934884fde27e9faaa6501f2923
Clarisse Amaral Bussolotti <clariamalotti@gmail.com>
2026-08-26 17:27:37 -0300

    add: pasta de mobile

---
+++ Desenvolvimento Mobile/blank_standalone/app1/src/app/home/home.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { codeSlashOutline } from 'ionicons/icons';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon],
+})
+export class HomePage {
+  constructor() {
+    addIcons({codeSlashOutline});
+  }
+}
